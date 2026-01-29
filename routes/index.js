b263c6c8428c40b87e7113c89fcc6fa22886c388
Maulana <makhmadnaufal2506@gmail.com>
2026-01-29 12:55:07 +0700

    chore(user-auth): create login and landing page

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/controller')
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers/controller");
 
-router.get('/', (req, res) => {
-  res.redirect('/login') // atau /login
-})
+router.get("/", (req, res) => {
+  res.redirect("/login"); // atau /login
+});
 
-router.get('/login', Controller.loginForm)
-router.post('/login', Controller.login)
-router.get('/logout', Controller.logout)
+router.get("/regist", Controller.registForm);
+router.post("/regist", Controller.regist);
 
-router.get('/patients', Controller.patients)
-router.get('/patients/add', Controller.addPatientForm)
-router.post('/patients/add', Controller.addPatient)
+router.get("/login", Controller.loginForm);
+router.post("/login", Controller.login);
+router.get("/logout", Controller.logout);
 
-router.get('/doctors', Controller.doctors)
-router.get('/doctors/:id', Controller.doctorDetail)
+router.get("/patients", Controller.patients);
+router.get("/patients/add", Controller.addPatientForm);
+router.post("/patients/add", Controller.addPatient);
 
-router.get('/appointments', Controller.appointments)
-router.get('/appointments/add', Controller.addAppointmentForm)
-router.post('/appointments/add', Controller.addAppointment)
-router.get('/appointments/:id', Controller.appointmentDetail)
-router.get('/appointments/:id/delete', Controller.deleteAppointment)
+router.get("/doctors", Controller.doctors);
+router.get("/doctors/:id", Controller.doctorDetail);
 
-module.exports = router
+router.get("/appointments", Controller.appointments);
+router.get("/appointments/add", Controller.addAppointmentForm);
+router.post("/appointments/add", Controller.addAppointment);
+router.get("/appointments/:id", Controller.appointmentDetail);
+router.get("/appointments/:id/delete", Controller.deleteAppointment);
+
+module.exports = router;
